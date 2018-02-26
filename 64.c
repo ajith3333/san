7e65a8152bb5c8a855d236d34ff709b14907693d
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-02-26 14:05:48 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    clrscr();
+printf("Enter an integer: ");
+    scanf("%d", &number);
+    (number % 2 == 0) ? printf("%d is even.", number) : printf("%d is odd.", number);
+     return 0;
+     }
